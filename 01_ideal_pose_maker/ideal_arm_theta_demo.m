7cea344296f4083fe535d6aa0948af1825ab3a72
Shunya Hara <r115159@g.kobe-kosen.ac.jp>
2022-10-04 16:34:24 +0900

    初期姿勢の削除

--- 01_ideal_pose_maker/ideal_arm_theta_demo.m
+++ 01_ideal_pose_maker/ideal_arm_theta_demo.m
@@ -3,9 +3,12 @@
 %10リンクワイヤー指モデル　準静的問題
 
 clear all
-ideal_pat = [1 2 3 4 5];
 
 
+ideal_pat_num=3; %目標姿勢の数
+ideal_pat_all_num=ideal_pat_num+1; %目標姿勢+ダミー目標姿勢の数
+
+ideal_pat = [1:ideal_pat_all_num];
 pat_n = size(ideal_pat,2);      %パターン数
 
 %m = 100;
@@ -30,11 +33,13 @@ l_pa0(2,1) = 0;
 
 theta_r = zeros(20,m);
 filename_1 = sprintf('output/ideal_theta');
+%%
+%%%目標姿勢の作成%%%
 
     %01 初期姿勢
-    for i = 1:m
-       theta_r(1,i) = 0; 
-    end
+%     for i = 1:m
+%        theta_r(1,i) = 0; 
+%     end
     
     % exp wave
 %     phase=[0.4,0.7,1.0];
@@ -63,34 +68,38 @@ filename_1 = sprintf('output/ideal_theta');
 %     end
     
     % zerosum exp wave
-    phase=[0.4,0.7,1.0];
-    for j=2:4
+    phase=[0.3,0.6,0.9];
+    k=1;
+    for j=1:3
         theta_r_tmp=zeros(m);
         for i=1:m
-            theta_r_tmp(i)=atan2(wave_exp((i)/m,phase(j-1))-wave_exp((i-1)/m,phase(j-1)),1/m)*180/pi;
+            theta_r_tmp(i)=atan2(wave_exp((i)/m,phase(k))-wave_exp((i-1)/m,phase(k)),1/m)*180/pi;
         end
         theta_r(j,1)=theta_r_tmp(1);
         for i=2:m
             theta_r(j,i)=theta_r_tmp(i)-theta_r_tmp(i-1);
         end
+        k=k+1;
     end
-    
+        %ダミー姿勢
     theta_r_tmp=zeros(m);
-    for j=5:7
+    k=1;
+    for j=1:ideal_pat_num
         for i=1:m
-            theta_r_tmp(i)=theta_r_tmp(i)+atan2(-wave_exp((i)/m,phase(j-1-3))+wave_exp((i-1)/m,phase(j-1-3)),1/m)*180/pi;
+            theta_r_tmp(i)=theta_r_tmp(i)+atan2(-wave_exp((i)/m,phase(k))+wave_exp((i-1)/m,phase(k)),1/m)*180/pi;
         end
+        k=k+1;
     end
-    j=5;
+    j=ideal_pat_all_num;
     theta_r(j,1)=theta_r_tmp(1);
     for i=2:m
         theta_r(j,i)=theta_r_tmp(i)-theta_r_tmp(i-1);
     end
     
-    
+ 
+ 
+%%
 theta_r = theta_r * pi/180;%[rad]
-
-
 for num = 1:pat_n
     %%
     %%plot＿理想最終状態%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
